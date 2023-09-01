5dc4f24167e7c473bb9ba4b8e1ef7755cc13e3c7
diniduthumula <diniduthumula@gmail.com>
2023-09-01 15:11:56 +0530

    first commit

---
+++ src/supermarket/layered/SupermarketLayered.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package supermarket.layered;
+
+/**
+ *
+ * @author Macmillan
+ */
+public class SupermarketLayered {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+    
+}
